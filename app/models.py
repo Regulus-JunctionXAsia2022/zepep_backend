d657a0383c3a804e9b525a345b756de970b68a4b
James Jung <therealjamesjung@gmail.com>
2022-08-20 14:32:17 +0900

    feat: add defaul value for friendship

--- app/models.py
+++ app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Zepep(models.Model):
     name = models.CharField(max_length=128)
     user = models.CharField(max_length=128)
-    friendship = models.IntegerField()
+    friendship = models.IntegerField(default=0)
