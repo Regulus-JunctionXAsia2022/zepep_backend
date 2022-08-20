8a4621aaf821bfa5b2b50effbf44bc8f7f7e3ea3
James Jung <therealjamesjung@gmail.com>
2022-08-20 23:32:53 +0900

    feat: add created at field to Zepep

--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ class Zepep(models.Model):
     name = models.CharField(max_length=128)
     user = models.CharField(max_length=128)
     friendship = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
